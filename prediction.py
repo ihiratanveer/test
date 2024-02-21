90a3655d1efce069200c026283ea0c16f321dacb
Hira Tanveer <33114673+ihiratanveer@users.noreply.github.com>
2024-02-21 22:14:26 +0500

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,6 +1,7 @@
 import joblib
-
+# import tensorflow
 
 def predict(data):
-    clf = joblib.load("rf_model.sav")
-    return clf.predict(data)
+    # clf = tensorflow.keras.models.load_model("model.h5")
+    # print(clf)
+    return "yes your name is hira"
